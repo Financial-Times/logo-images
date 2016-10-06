2dcf8f9106a2816ebe3748a93777e01f863307fb
Alice Bartlett <alice.bartlett@ft.com>
2016-10-06 17:09:31 +0100

    Make this image set appear in the registry
    - Add and Origami.json
    - Add an imageList.json
    - Add a gulp task to keep the image list up to date

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict'; //eslint-disable-line
+
+const gulp = require('gulp');
+const fs = require('fs');
+
+// Read svg directory for list of logo names to pass to demo template
+const logoNames = fs.readdirSync('src').filter((file) => {
+
+	// File is an SVG
+	return file.indexOf(".svg") > -1;
+
+}).map((file) => {
+	// Removes .svg extension
+	return file.slice(0, -4);
+});
+
+gulp.task('listImages', () => {
+	const logos = logoNames.map((file) => {
+		return { name: file };
+	});
+
+	// Write the list of logos found in /svg to the data.json file.
+	fs.writeFileSync('imageList.json', JSON.stringify({ logos: logos }, null, '\t'), { encoding: 'utf-8' });
+});
+
+gulp.task('default', ['listImages']);
